a0e1fc55288a325fed3b7094e90d382967bd15f0
Pili Sánchez <sanchezpili6@gmail.com>
2022-05-07 20:54:59 -0500

    Show recipe and recipes list (harcoded)

--- recetario/src/components/RecipeImageComponent.vue
+++ recetario/src/components/RecipeImageComponent.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>Recipe Image</div>
+  <div class="image-container">
+    <picture>
+      <source media="(max-width: 991px)" :srcset="recipeImage">
+      <img :src="recipeImage" width="360" height="180">
+    </picture>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "RecipeImageComponent.vue"
+  name: "RecipeImageComponent.vue",
+  data:()=>{
+    return{
+      recipeImage: 'https://static01.nyt.com/images/2021/04/30/dining/ar-eggs-benedict/ar-eggs-benedict-articleLarge.jpg'
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  .image-container{
+    background: rgb(9,9,121);
+    background: linear-gradient(90deg, rgba(9,9,121,1) 0%, rgba(0,255,238,1) 48%, rgba(2,0,36,1) 100%);
+  }
 </style>
